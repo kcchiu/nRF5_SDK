afbc18d35bf533eaded62b142d7ccbf7cd39462f
kcchiu <kcchiu@livemail.tw>
2016-05-20 15:11:03 +0800

    Commit of original Nordic nRF5 SDK v11

---
+++ nRF5_SDK_v11.x.x/examples/ble_peripheral/ble_app_hrs/pca10040/ser_s132_uart/arm5_no_packs/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ble_app_hrs_s132_uart_pca10040' 
+ * Target:  'nrf52832_xxaa' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
